77d6c9e15ffbb5c5afa8a52bf9a23fd27fed14bc
MaxLyonel <leorexx100@gmail.com>
2021-09-21 21:00:33 -0400

    Agregar archivos de proyecto.

---
+++ Pregunta 4 f(sharp)/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let listaa = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
+
+let impares lista = 
+    let esImpar x = x%2 = 1
+    List.filter esImpar lista
+
+let pares lista =
+    let esPar x = x%2 = 0
+    List.filter esPar lista
+
+
+
+[<EntryPoint>]
+let main argv =
+    for i in pares listaa do
+        printfn "%i" i
+    printfn " "
+    for i in impares listaa do
+        printfn "%i" i
+    0 // return an integer exit code
